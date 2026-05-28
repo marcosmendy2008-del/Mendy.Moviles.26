d919321353e293244cc3bf450203d5a1f56c4bff
lourdes222 <lourdes.munoz666222@gmail.com>
2026-05-28 18:42:17 -0300

    inicio

--- app/(tabs)/cajascoloridas.tsx
+++ app/(tabs)/cajascoloridas.tsx
@@ -1,5 +1,55 @@
-import { Text, View} from 'react-native';
-export default function cajascoloridas()
-{
-    
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+      return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>¡Hola mundo!</Text>
+
+      <View style={styles.filas}>
+        <View style={styles.caja1} />
+        <View style={styles.caja2} />
+        <View style={styles.caja3} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },    
+
+  titulo: {
+    fontSize: 24,
+    color: 'blue',
+    marginBottom: 30,
+  },
+
+  filas: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+  caja1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+
+  caja2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+  },
+
+  caja3: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'cyan',
+  },
+});
